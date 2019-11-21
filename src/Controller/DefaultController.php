f52a2cdfb7fbeda5508c2d8ff5685823d4e27450
Alexandre Waeles <5228298+Axelandre42@users.noreply.github.com>
2019-11-21 20:43:15 +0100

    Added index controller and route with placeholder.

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends AbstractController
+{
+
+    public function index()
+    {
+        return new Response('It works!', Response::HTTP_OK, [
+            'Content-Type' => 'text/plain'
+        ]);
+    }
+}
+
