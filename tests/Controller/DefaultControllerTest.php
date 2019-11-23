c79ad99d455dcffc22dd01b16ee39b5175ea95ef
Alexandre Waeles <5228298+Axelandre42@users.noreply.github.com>
2019-11-23 13:32:20 +0100

    Clean function name

--- tests/Controller/DefaultControllerTest.php
+++ tests/Controller/DefaultControllerTest.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 class DefaultControllerTest extends WebTestCase
 {
 
-    public function testShowPost()
+    public function testIndex()
     {
         $client = static::createClient();
 
