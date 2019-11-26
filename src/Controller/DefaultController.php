5078627e835a8ba10071b51e804f9fc89b8b8c24
Alexandre Waeles <5228298+Axelandre42@users.noreply.github.com>
2019-11-26 21:24:30 +0100

    Fixed bad location in DefaultController.php

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -9,6 +9,6 @@ class DefaultController extends AbstractController
 
     public function index()
     {
-        return $this->render("index.html.twig");
+        return $this->render("default/index.html.twig");
     }
 }
